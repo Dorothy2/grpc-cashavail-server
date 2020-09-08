c6e1e1979a925271f1a0b61d8ec53e82dc79f0b5
Dorothy Rifai <drifai28@gmail.com>
2020-09-08 12:36:29 -0400

    Stream (multiple) version working

--- src/main/java/com/drifai/CashAvailService.java
+++ src/main/java/com/drifai/CashAvailService.java
@@ -1,6 +1,9 @@
 package com.drifai;
 
 
+import java.util.ArrayList;
+import java.util.List;
+
 import com.drifai.dto.CashAvailable;
 import com.drifai.grpc.Cashavail.APIResponse;
 import com.drifai.grpc.Cashavail.CashAvailRequest;
@@ -38,8 +41,51 @@ public class CashAvailService extends cashAvailImplBase {
 		responseObserver.onCompleted();
 	}
 	
+	@Override
+	public void getCashAvailStream(CashAvailRequest request, StreamObserver<APIResponse> responseObserver) {
+		
+		System.out.println("\nRequest coming in: " + request.toString());
+		
+		
+		//try {
+			List<CashAvailable> fakedData = getMultipleFakedData();
+
+		    for (CashAvailable cash : fakedData) {
+		    	APIResponse response =
+		    			APIResponse.newBuilder()
+		    			.setClientId(cash.getClientId())
+		    			.setFundId(cash.getFundId())
+		    			.setCashAvailType(cash.getCashAvailType())
+		    			.setCashAvailSubType(cash.getCashAvailSubtype())
+		    			.setKnowledgeDate(cash.getKnowledgeDate())
+		    			.setEffectiveDate(cash.getEffectiveDate())
+		    			.setCurrency(cash.getCurrency())
+		    			.setAmount(cash.getAmount())
+		    			.build();
+		    	responseObserver.onNext(response);
+		    }
+		  //} catch (RuntimeException e) {
+		    // Cancel RPC
+		  //  requestObserver.onError(e);
+		  //  throw e;
+		  //}
+		
+		
+		
+		responseObserver.onCompleted();
+	}
+	
+
+	
 	private CashAvailable getFakedData() {
 		return new CashAvailable("12345", "3C", "CapStock", "CPSTKCLR", "2020-09-06", "2020-09-06", "EUR", -3500.00);
+	}
+	
+	private List<CashAvailable> getMultipleFakedData() {
+		List<CashAvailable> list = new ArrayList<>();
+		list.add(new CashAvailable("12345", "3C", "CapStock", "CPSTKCLR", "2020-09-06", "2020-09-06", "EUR", -3500.00));
+		list.add(new CashAvailable("12345", "3C", "CapStock", "CPSTKCLR", "2020-09-06", "2020-09-06", "EUR", -2500.00));
+		return list;
 	}
 	
 	
